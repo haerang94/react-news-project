c0d8c0e3c7d9d63421f2d21ebee3855fe1f4dbfa
haerang <gofkdvjvl@naver.com>
2020-12-01 21:47:20 +0900

    feat:로딩중 화면 가운데 배치

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,16 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 import NewsContainer from "containers/NewsContainer";
 import SearchContainer from "containers/SearchContainer";
 import PageContainer from "containers/PageContainer";
 import HeaderContainer from "containers/HeaderContainer";
+
+const PageWrapper = styled.section`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 const MainPage = React.memo(() => {
   return (
-    <div>
+    <PageWrapper>
       <HeaderContainer />
       <SearchContainer />
       <NewsContainer />
       <PageContainer />
-    </div>
+    </PageWrapper>
   );
 });
 
