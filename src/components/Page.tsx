c59d0a360b79fbcbe21f091785a43dce04f6a165
haerang <gofkdvjvl@naver.com>
2020-12-01 18:12:18 +0900

    feat:page 버튼 구현

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,26 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Button } from "components/sharedComponents";
+import { RightArrow } from "@styled-icons/boxicons-solid/RightArrow";
+import { LeftArrow } from "@styled-icons/boxicons-solid/LeftArrow";
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+`;
+const NewButton = styled(Button)`
+  width: 60px;
+  margin: 5px;
+`;
+const Right = styled(RightArrow)`
+  width: 20px;
+  height: 20px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+const Left = styled(LeftArrow)`
+  width: 20px;
+  height: 20px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 interface Props {
   totalResults: number | null;
   onPageMove: (text: string, page?: number) => void;
   keyword: string | null;
 }
-
-const NewButton = styled(Button)``;
-
-const Page = ({ totalResults, onPageMove, keyword }: Props) => {
+const Page = React.memo(({ totalResults, onPageMove, keyword }: Props) => {
   const [page, SetPage] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
 
   return (
-    <div>
+    <Container>
+      <Left />
       {Array.from(Array(5), (_, idx) => (
-        <NewButton>{page + idx}</NewButton>
+        <NewButton key={`page-btn-${idx}`}>{page + idx}</NewButton>
       ))}
-    </div>
+      <Right />
+    </Container>
   );
-};
+});
 
 export default Page;
