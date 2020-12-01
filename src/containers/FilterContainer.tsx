a235766dd01198bf72073e2a41fc32291dcf9952
haerang <gofkdvjvl@naver.com>
2020-12-02 06:01:37 +0900

    feat:정렬함수 생성

--- src/containers/FilterContainer.tsx
+++ src/containers/FilterContainer.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 import Filter from "components/Filter";
-
+import useNews from "customHooks/useNews";
+import { Article } from "types/article";
 const FilterContainer = () => {
+  const { data } = useNews();
+  const date_descending = (a: Article, b: Article) => {
+    const x = new Date(a.publishedAt).getTime();
+    const y = new Date(b.publishedAt).getTime();
+    return x < y ? 1 : -1;
+  };
+  if (data) {
+    console.log(data.sort(date_descending));
+  }
+
   return <Filter></Filter>;
 };
 
