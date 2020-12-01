1dc8939e414419768388885c495d6e098d7dd588
haerang <gofkdvjvl@naver.com>
2020-12-01 19:16:42 +0900

    feat: 다음 이전 넘어가는 버튼 구현

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -34,22 +34,57 @@ interface Props {
   onPageMove: (text?: string | null, page?: number) => void;
   keyword: string | null;
 }
+function calculate(totalResults: number | null) {
+  let lastIdx;
+  if (totalResults === null) {
+    lastIdx = 0;
+  } else {
+    lastIdx =
+      totalResults % 6 === 0
+        ? totalResults / 6
+        : parseInt(totalResults / 6 + "") + 1;
+  }
+  return lastIdx;
+}
+
 const Page = React.memo(({ totalResults, onPageMove, keyword }: Props) => {
-  const [page, SetPage] = useState(1);
+  const [page, setPage] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
+  const totalPage = calculate(totalResults);
+
+  const onClickLeft = () => {
+    if (page === 1) return;
+    setPage((page) => page - 5);
+  };
+
+  const onClickRight = () => {
+    if (page + 5 > totalPage) return;
+    setPage((page) => page + 5);
+  };
+
+  const onCurrentPage = useCallback(
+    (idx) => {
+      onPageMove(keyword, idx);
+      setCurrentPage((currentPage) => idx);
+    },
+    [onPageMove, keyword]
+  );
 
   return (
     <Container>
-      <Left />
-      {Array.from(Array(5), (_, idx) => (
-        <NewButton
-          key={`page-btn-${idx}`}
-          onClick={() => onPageMove(keyword, page + idx)}
-        >
-          {page + idx}
-        </NewButton>
-      ))}
-      <Right />
+      <Left onClick={onClickLeft} />
+      {Array.from(Array(5), (_, idx) => {
+        if (page + idx <= totalPage)
+          return (
+            <NewButton
+              key={`page-btn-${idx}`}
+              onClick={() => onCurrentPage(page + idx)}
+            >
+              {page + idx}
+            </NewButton>
+          );
+      })}
+      <Right onClick={onClickRight} />
     </Container>
   );
 });
