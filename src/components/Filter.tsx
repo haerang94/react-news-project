73dbe3b2381eb33c3198d4975d81b763e32bd5a1
haerang <gofkdvjvl@naver.com>
2020-12-02 05:40:31 +0900

    feat:정렬 컴포넌트 생성

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & > * {
+    margin: 5px;
+  }
+`;
+const Label = styled.label``;
+
+const Select = styled.select`
+  width: 100px;
+  height: 20px;
+`;
 
 const Filter = () => {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <Label htmlFor="filter">Sort</Label>
+      <Select>
+        <option value="date">Date</option>
+        <option value="source">Source</option>
+      </Select>
+    </Wrapper>
+  );
 };
 
 export default Filter;
